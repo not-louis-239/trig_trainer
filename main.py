a396badf9f7e986bb2b5ed1b9f7af82d1e260fd4
not-louis-239 <louis.boulton@icloud.com>
2026-05-28 19:12:25 +0800

    Add .gitignore file and basic program loop

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-
+import pygame as pg
 
 def main():
-    ...
+    scr = pg.Surface((800, 600))
+    clock = pg.time.Clock()
+
+    while True:
+        events = pg.event.get()
+        dt_s = clock.tick(60) / 1_000
+
+        for event in events:
+            if event.type == pg.QUIT:
+                return
+
+        scr.fill((255, 255, 255))
+        pg.display.flip()
 
 if __name__ == "__main__":
     main()
